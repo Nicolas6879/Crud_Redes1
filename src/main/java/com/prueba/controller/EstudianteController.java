3dec9899fc9e40bf2c979c4e92ecb9b4a689df40
Nicolas6879 <juan2210050@correo.uis.edu.co>
2023-11-27 13:52:21 -0500

    Creacion y funcionalidad de crear estudiante

--- src/main/java/com/prueba/controller/EstudianteController.java
+++ src/main/java/com/prueba/controller/EstudianteController.java
@@ -1,9 +1,29 @@
 package com.prueba.controller;
 
+import com.prueba.model.estudiante;
+import com.prueba.service.EstudianteService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/estudiante")
 public class EstudianteController {
+    //public String create(){
+      //  return
+    //}
+    private final Logger LOGGER = LoggerFactory.getLogger(EstudianteController.class);
+    @Autowired
+    private EstudianteService estudianteService;
+
+    @PostMapping("/save")
+    public String save(estudiante estudiante){
+        LOGGER.info("Este es el objeto estudiante {}",estudiante);
+        estudianteService.save(estudiante);
+        return "redirect:/estudiante";
+    }
+
+
 }
