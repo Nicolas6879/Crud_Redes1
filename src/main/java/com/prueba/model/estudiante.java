5c5e281b0af4dea3180b21816ad7b50cd71a91c7
Nicolas Garcia <juan2210050@correo.uis.edu.co>
2023-11-27 01:32:35 -0500

    Creacion de la estructura del proyecto y las clases model

---
+++ src/main/java/com/prueba/model/estudiante.java
@@ -0,0 +1,82 @@
+package com.prueba.model;
+
+public class estudiante {
+    private Integer codigo;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String carrera;
+    private String ciudad;
+
+    public estudiante() {
+    }
+
+    public estudiante(Integer codigo, String nombre, String apellido, String correo, String carrera, String ciudad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.carrera = carrera;
+        this.ciudad = ciudad;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "estudiante{" +
+                "codigo=" + codigo +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", correo='" + correo + '\'' +
+                ", carrera='" + carrera + '\'' +
+                ", ciudad='" + ciudad + '\'' +
+                '}';
+    }
+}
